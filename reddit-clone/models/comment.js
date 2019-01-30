031d2b7a4e5dd7833e0e7e215a948943e978fe42
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-29 16:40:45 -0800

    [feature]comment model

---
+++ reddit-clone/models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  title:    {type: String, required: true},
+  comment:  {type: String, required: true},
+  postId:   {type: Schema.Types.ObjectId, ref: 'post' }
+});
+
+let comment = mongoose.model('comment', commentSchema)
+
+module.exports = comment;
