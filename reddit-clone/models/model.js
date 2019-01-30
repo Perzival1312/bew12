9f3dd01934c8ec51dd2156ff51482995618ed648
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-29 16:44:13 -0800

    0

--- reddit-clone/models/model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const thingSchema = new Schema({
-  name: {type: String, required:true},
-})
-let thing = mongoose.model('thing', thingSchema)
-
-module.exports = thing;
