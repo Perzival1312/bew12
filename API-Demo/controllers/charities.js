060f98c792317b081146f5edc5de97d3de962938
Perzival1312 <jj.mongoose11@hotmail.com>
2019-01-30 14:27:41 -0800

    stuff

--- API-Demo/controllers/charities.js
+++ API-Demo/controllers/charities.js
@@ -3,9 +3,31 @@ const router = express.Router();
 const Code = require('../models/charity');
 // get the models required in here...
 
+var world = {
+  "trainer" : {
+    "stats" : ["height", "age", "Name"],
+    "pokemon" : ["Charmander", "Arceus", "Pickachu"] 
+  }  
+}
+
 // INDEX
 router.get('/', (req, res) => {
-  res.render('index')
+  res.send(world)
 });
 
+router.get('/pokemon/new', (req, res) => {
+  world["trainer"]["pokemon"].push("Bulbasaur")
+  res.send(world)
+});
+
+router.get('/pokemon/remove', (req, res) => {
+  world["trainer"]["pokemon"].remove("Bulbasaur")
+})
+
+
+
 module.exports = router
+
+
+
+
